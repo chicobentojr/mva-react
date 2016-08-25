ff3234a87ef199e7660b28f5c7b0ca1f7d5956b3
Francisco Bento <francisco.bento.jr@hotmail.com>
2016-08-25 19:07:14 -0300

    2.3 Introducing JSX

---
+++ app/app.js
@@ -0,0 +1,53 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <div id="header"></div>
+        <div className="container">
+          <div className="column">
+            <InboxPane />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var InboxPane = React.createClass({
+  render: function(){
+    return(
+      <div id="inbox-pane">
+        <h1>Inbox</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Chat Received</th>
+              <th>Name</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <InboxItem />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+});
+
+var InboxItem = React.createClass({
+  render: function(){
+    return (
+      <tr>
+        <td>5pm</td>
+        <td>Rami loves pizza</td>
+        <td>Confirmed</td>
+      </tr>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("main"));
